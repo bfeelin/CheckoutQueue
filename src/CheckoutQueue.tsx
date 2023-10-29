037693bf2936642cd20b20e062c9510a3452bfc2
Brian Phelan <brianphelan59@gmail.com>
2023-10-29 10:06:35 -0400

    handleCheckout, handle scanning items

--- src/CheckoutQueue.tsx
+++ src/CheckoutQueue.tsx
@@ -1,18 +1,62 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 type Data = Array<Array<number>>
 
-export default function CheckoutQueue ({ data }: { data: Data }) {
-    const [newShopper, setNewShopper] = useState<string>('')
+export default function CheckoutQueue ({ initialData }: { initialData: Data }) {
+    const [data, setData] = useState(initialData)
+    const [newShopper, setNewShopper] = useState<number|string>('')
 
     function handleCheckout(){
+        let shortestQueue:null|number = null
+        let shortestQueueLength:null|number = null
 
+        data.forEach((queue, index) => {
+            let thisQueueItems = queue.reduce((prev, curr) => prev + curr, 0)
+            if(!shortestQueue || !shortestQueueLength || thisQueueItems < shortestQueueLength){
+                shortestQueue = index
+                shortestQueueLength = thisQueueItems
+            }
+        })
+
+        let newData = [...data]
+        if(typeof shortestQueue == "number" && typeof newShopper == "number") newData[shortestQueue].push(newShopper)
+        setData(newData)
+        setNewShopper('')
     }
+    useEffect(() => {
+        // This function will run every 0.5 seconds
+        const intervalId = setInterval(() => {
+            setData((prevData) => {
+              const newData = prevData.map((queue) => {
+                if(queue.length > 0){
+                    const shopper = queue[0];
+                    let newQueue = [...queue]
+                    if (shopper <= 1) {
+                      // Remove the first shopper
+                      return newQueue.slice(1);
+                    } else {
+                      // Decrement the shopper's item count by 1
+                      newQueue[0] = shopper - 1;
+                      return newQueue;
+                    }
+                }
+                else return queue
+              });
+              console.log(newData)
+              return newData;
+            });
+        }, 500); // 500 milliseconds = 0.5 seconds
+    
+        // Clear the interval when the component unmounts to prevent memory leaks
+        return () => {
+          clearInterval(intervalId);
+        };
+      }, []);
 
     return(
         <>
-            <input value={newShopper} onChange={(e) => setNewShopper(e.target.value)}></input>
-            <button onClick={() => handleCheckout()}>Checkout</button>
+            <input type="number" value={newShopper} onChange={(e) => setNewShopper(parseInt(e.target.value))}></input>
+            <button disabled={!newShopper} onClick={() => handleCheckout()}>Checkout</button>
             <div className="queues">
             {data?.map((queue, index) => (
                 <div className='queue' key={`queue-wrapper-${index}`}>
