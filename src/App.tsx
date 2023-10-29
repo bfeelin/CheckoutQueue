b7c2e7bbaccf57222d188743c5c21191b8254e5c
Brian Phelan <brianphelan59@gmail.com>
2023-10-29 09:04:28 -0400

    Removed boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CheckoutQueue from './CheckoutQueue'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = [
+    [3, 2 ,6],
+    [4, 6],
+    [2, 9],
+    [8, 5, 9],
+    [3, 5],
+  ]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CheckoutQueue data={data} />
     </>
   )
 }
